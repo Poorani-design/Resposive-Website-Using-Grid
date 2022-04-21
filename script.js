9958c106b9e42c74d322805dba4b4681a13d0ac4
POORANI GUNASEKAREN <arulpoorani88@gmail.com>
2022-04-21 12:25:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+var button = document.querySelector(".bar"),links = document.querySelector(".nav-links");
+
+   button.addEventListener("click",()=>{
+     links.classList.toggle("display-navbar");
+     
+   button.children[0].classList.toggle("toggle1");
+   button.children[1].classList.toggle("toggle2");
+   button.children[2].classList.toggle("toggle3");
+   })
+</script>
